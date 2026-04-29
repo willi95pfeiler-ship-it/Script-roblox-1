2eec5dcf79eb69b2e8d8e9c80ae2ad9e5e23eea8
willi95pfeiler-ship-it <willi95.pfeiler@gmail.com>
2026-04-29 12:14:37 +0200

    Update script.lua

--- script.lua
+++ script.lua
@@ -1,1 +1,16 @@
-game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = 100
+local player = game.Players.LocalPlayer
+local char = player.Character or player.CharacterAdded:Wait()
+
+-- Attendre que tout soit chargé
+wait(1)
+
+for _, v in pairs(char:GetDescendants()) do
+    if v:IsA("Motor6D") then
+        -- Modifier la position des joints
+        v.C0 = v.C0 * CFrame.Angles(
+            math.rad(math.random(-30,30)),
+            math.rad(math.random(-30,30)),
+            math.rad(math.random(-30,30))
+        )
+    end
+end
